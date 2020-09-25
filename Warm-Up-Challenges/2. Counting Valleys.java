fbd823bb2e07b72ce398f4cbc77b62661ba6eb23
Aman Shekhar <Aman Shekhar>
2020-09-25 20:22:05 +0530

    'hacker-rank-warm-up-challenges'

---
+++ Warm-Up-Challenges/2. Counting Valleys.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String s) {
+    // Write your code here
+        int altitude = 0;
+        int num_valleys = 0;
+
+        for(int i =0; i<steps; i++) {
+            if(s.charAt(i) == 'U') {
+                if(altitude == -1) {
+                    num_valleys++;
+                }
+                altitude++;
+            }
+            if(s.charAt(i) == 'D') {
+                altitude--;
+            }
+        }
+        return num_valleys;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
